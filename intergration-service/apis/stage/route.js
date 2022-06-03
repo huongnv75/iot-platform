70543b74f745984aabf9baf1a867ccf0f3490c99
huongnv75 <huongnv75@outlook.com>
2022-06-03 07:01:03 +0700

    update add,remove, addwith in stage

--- intergration-service/apis/stage/route.js
+++ intergration-service/apis/stage/route.js
@@ -5,5 +5,8 @@ router.get('/', controller.list)
     .get('/:id', controller.getById)
     .post('/', controller.add)
     .put('/:id', controller.update)
-    .delete('/:id', controller.delete);
+    .delete('/:id', controller.delete)    
+    .post('/add-with-errors', controller.addWithErrors)
+    .put('/add-error', controller.addError)
+    .put('/remove-error', controller.removeError);
 module.exports = router;
