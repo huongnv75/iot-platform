aba5455757068604b4e7ee0970bf5f7ef8c23b05
huongnv75 <huongnv75@outlook.com>
2022-07-13 15:56:20 +0700

    check roles length >0

--- integration-service/cron-job/syncUser.js
+++ integration-service/cron-job/syncUser.js
@@ -155,7 +155,7 @@ function synchroTenantUsersDatabase() {
     });
 }
 
-function synchroUsersDatabase(){
+function synchroUsersDatabase() {
     scada.getToken(config.scada.sysadmin, config.scada.sysadminPassword).then(scadaTokenSysAdmin => {
         scada.deleteGoogleTenant(scadaTokenSysAdmin);
         scada.getTenantInfos(scadaTokenSysAdmin, config.scada.tenantName).then(scadaTenants => {
@@ -171,110 +171,112 @@ function synchroUsersDatabase(){
                                     for (const group of keycloakGroups) {
                                         // check group nếu có quyền admin
                                         keycloak.getGroupRoleMappings(keycloakToken, group.id).then(roles => {
-                                            if(roles.includes('admin_scada')){
-                                                keycloak.getGroupMembers(keycloakToken, group.id).then(keycloakUsers => {
-                                                    let email = null;
-                                                    // kiểm tra xem trong keycloak có user nào thêm mà scada chưa thêm thì thêm
-                                                    // user nào đã có mà chưa cập nhật dashboard default thì cập nhật
-                                                    for (const user of keycloakUsers) {
-                                                        email = user.email;
-                                                        if (email == null || email == '') {
-                                                            email = user.username + '@gmail.com';
-                                                        }
-                                                        if (!global.contain(oldEmails, email)) {
-                                                            scada.createTenantUser(scadaTokenSysAdmin, scadaTenant.id, email);
-                                                        } else {
-                                                            // cập nhật user tenant đã tồn tại
-                                                        }
-                                                    }
-                                                    // kiểm tra trong user cũ của scada có user nào chưa xóa thì xóa
-                                                    const emailKeycloaks = keycloakUsers.map(function (a) {
-                                                        let email = a.email;
-                                                        if (email == null || email == '') email = a.username + 'gmail.com';
-                                                        return email;
-                                                    });
-                                                    for (const element of oldUsers) {
-                                                        if (!global.contain(emailKeycloaks, element.email)) {
-                                                            // xóa user tenant
-                                                        }
-                                                    }
-                                                })
-                                            } else {
-                                                // tạo mới customer và user nếu như chưa có trên scada
-                                                if (!global.contain(customerNames, group.name)) {
-                                                    scada.createCustomer(scadaToken, group.name).then(customer => {
-                                                        keycloak.getGroupMembers(keycloakToken, group.id).then(keycloakMembers => {
-                                                            for (const member of keycloakMembers) {
-                                                                let email = member.email;
-                                                                if (email == null || email == '') {
-                                                                    email = member.username + '@gmail.com';
-                                                                }
-                                                                scada.createCustomerUser(scadaToken, customer, email).then(u => {
-                                                                    scada.activeUser(scadaToken, u).then(rs => {
-                    
-                                                                    });
-                                                                })
-                    
+                                            if (roles.length > 0) {
+                                                if (roles.includes('admin_scada')) {
+                                                    keycloak.getGroupMembers(keycloakToken, group.id).then(keycloakUsers => {
+                                                        let email = null;
+                                                        // kiểm tra xem trong keycloak có user nào thêm mà scada chưa thêm thì thêm
+                                                        // user nào đã có mà chưa cập nhật dashboard default thì cập nhật
+                                                        for (const user of keycloakUsers) {
+                                                            email = user.email;
+                                                            if (email == null || email == '') {
+                                                                email = user.username + '@gmail.com';
                                                             }
+                                                            if (!global.contain(oldEmails, email)) {
+                                                                scada.createTenantUser(scadaTokenSysAdmin, scadaTenant.id, email);
+                                                            } else {
+                                                                // cập nhật user tenant đã tồn tại
+                                                            }
+                                                        }
+                                                        // kiểm tra trong user cũ của scada có user nào chưa xóa thì xóa
+                                                        const emailKeycloaks = keycloakUsers.map(function (a) {
+                                                            let email = a.email;
+                                                            if (email == null || email == '') email = a.username + 'gmail.com';
+                                                            return email;
                                                         });
-                    
-                                                    });
+                                                        for (const element of oldUsers) {
+                                                            if (!global.contain(emailKeycloaks, element.email)) {
+                                                                // xóa user tenant
+                                                            }
+                                                        }
+                                                    })
                                                 } else {
-                                                    //customer đã có trên scada ==> cần cập nhật user của customer đó theo group của keycloak
-                                                    let match = scadaCustomers.find(obj => {
-                                                        return obj.name == group.name;
-                                                    });
-                                                    scada.detailCustomer(scadaToken, match.id).then((customer) => {
-                                                        scada.getCustomerUsers(scadaToken, customer.id.id).then(oldUsers => {
-                                                            const oldEmails = oldUsers.map(function (a) { return a.email; });
-                                                            keycloak.getGroupMembers(keycloakToken, group.id).then(keycloakUsers => {
-                                                                let email = null;
-                                                                // kiểm tra xem trong keycloak có user nào thêm mà scada chưa thêm thì thêm
-                                                                // user nào đã có mà chưa cập nhật dashboard default thì cập nhật
-                                                                for (const user of keycloakUsers) {
-                                                                    email = user.email;
+                                                    // tạo mới customer và user nếu như chưa có trên scada
+                                                    if (!global.contain(customerNames, group.name)) {
+                                                        scada.createCustomer(scadaToken, group.name).then(customer => {
+                                                            keycloak.getGroupMembers(keycloakToken, group.id).then(keycloakMembers => {
+                                                                for (const member of keycloakMembers) {
+                                                                    let email = member.email;
                                                                     if (email == null || email == '') {
-                                                                        email = user.username + '@gmail.com';
+                                                                        email = member.username + '@gmail.com';
                                                                     }
-                                                                    if (!global.contain(oldEmails, email)) {
-                                                                        scada.createCustomerUser(scadaToken, customer, email);
-                                                                    } else {
-                                                                        let user = oldUsers.find(obj => {
-                                                                            return obj.email == email;
+                                                                    scada.createCustomerUser(scadaToken, customer, email).then(u => {
+                                                                        scada.activeUser(scadaToken, u).then(rs => {
+
                                                                         });
-                                                                        if (user.additionalInfo.defaultDashboardId == null) {
-                                                                            scada.getCustomerDashboards(scadaToken, customer.id.id).then(dashboards => {
-                                                                                let db = dashboards.find(obj => {
-                                                                                    return obj.name == 'Giám sát';
-                                                                                });
-                                                                                if (db != undefined && db != null) {
-                                                                                    scada.updateDefaultDashboardUser(scadaToken, user, db.id).then(rs => {
-                    
+                                                                    })
+
+                                                                }
+                                                            });
+
+                                                        });
+                                                    } else {
+                                                        //customer đã có trên scada ==> cần cập nhật user của customer đó theo group của keycloak
+                                                        let match = scadaCustomers.find(obj => {
+                                                            return obj.name == group.name;
+                                                        });
+                                                        scada.detailCustomer(scadaToken, match.id).then((customer) => {
+                                                            scada.getCustomerUsers(scadaToken, customer.id.id).then(oldUsers => {
+                                                                const oldEmails = oldUsers.map(function (a) { return a.email; });
+                                                                keycloak.getGroupMembers(keycloakToken, group.id).then(keycloakUsers => {
+                                                                    let email = null;
+                                                                    // kiểm tra xem trong keycloak có user nào thêm mà scada chưa thêm thì thêm
+                                                                    // user nào đã có mà chưa cập nhật dashboard default thì cập nhật
+                                                                    for (const user of keycloakUsers) {
+                                                                        email = user.email;
+                                                                        if (email == null || email == '') {
+                                                                            email = user.username + '@gmail.com';
+                                                                        }
+                                                                        if (!global.contain(oldEmails, email)) {
+                                                                            scada.createCustomerUser(scadaToken, customer, email);
+                                                                        } else {
+                                                                            let user = oldUsers.find(obj => {
+                                                                                return obj.email == email;
+                                                                            });
+                                                                            if (user.additionalInfo.defaultDashboardId == null) {
+                                                                                scada.getCustomerDashboards(scadaToken, customer.id.id).then(dashboards => {
+                                                                                    let db = dashboards.find(obj => {
+                                                                                        return obj.name == 'Giám sát';
                                                                                     });
-                                                                                }
-                                                                            })
+                                                                                    if (db != undefined && db != null) {
+                                                                                        scada.updateDefaultDashboardUser(scadaToken, user, db.id).then(rs => {
+
+                                                                                        });
+                                                                                    }
+                                                                                })
+                                                                            }
                                                                         }
                                                                     }
-                                                                }
-                                                                // kiểm tra trong user cũ của scada có user nào chưa xóa thì xóa
-                                                                const emailKeycloaks = keycloakUsers.map(function (a) {
-                                                                    let email = a.email;
-                                                                    if (email == null || email == '') email = a.username + 'gmail.com';
-                                                                    return email;
-                                                                });
-                                                                for (const element of oldUsers) {
-                                                                    if (!global.contain(emailKeycloaks, element.email)) {
-                                                                        //xóa user
+                                                                    // kiểm tra trong user cũ của scada có user nào chưa xóa thì xóa
+                                                                    const emailKeycloaks = keycloakUsers.map(function (a) {
+                                                                        let email = a.email;
+                                                                        if (email == null || email == '') email = a.username + 'gmail.com';
+                                                                        return email;
+                                                                    });
+                                                                    for (const element of oldUsers) {
+                                                                        if (!global.contain(emailKeycloaks, element.email)) {
+                                                                            //xóa user
+                                                                        }
                                                                     }
-                                                                }
+                                                                })
                                                             })
                                                         })
-                                                    })
-                    
+
+                                                    }
                                                 }
                                             }
                                         });
-                                        
+
                                     }
                                 })
                             })
@@ -286,4 +288,4 @@ function synchroUsersDatabase(){
     })
 }
 
-module.exports = { synchroCustomerUsersDatabase, synchroTenantUsersDatabase , synchroUsersDatabase}
+module.exports = { synchroCustomerUsersDatabase, synchroTenantUsersDatabase, synchroUsersDatabase }
